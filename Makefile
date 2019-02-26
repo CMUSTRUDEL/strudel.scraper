01218fb697770621e4a8d0ca02fa58d4a9600259
Marat <user2589@users.noreply.github.com>
2019-02-26 15:23:09 -0500

    chore: configure automatic deployment

--- Makefile
+++ Makefile
@@ -5,15 +5,12 @@ TESTROOT = stscraper
 .PHONY: test
 test:
 	python -m unittest test
-	python3 -m unittest test
 
 .PHONY: publish
 publish:
-	$(MAKE) clean
-	$(MAKE) test
-	python setup.py sdist bdist_wheel
-	twine upload dist/*
-	$(MAKE) clean
+	test $$(git config user.name) || git config user.name "semantic-release (via TravisCI)"
+	test $$(git config user.email) || git config user.email "semantic-release@travis"
+	semantic-release publish
 
 .PHONY: clean
 clean:
@@ -26,7 +23,13 @@ clean:
 html:
 	sphinx-build -M html "docs" "docs/build"
 
+.PHONY: install
+install:
+	pip install -r requirements.txt
+
 .PHONY: install_dev
 install_dev:
-	pip install --user --upgrade -r requirements.txt
-	pip install --user --upgrade sphinx sphinx-autobuild
+	$(MAKE) install
+	pip install requests
+	pip install typing requests sphinx sphinx-autobuild
+	pip install python-semantic-release==3.11.2
--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
+import os
+import re
 from setuptools import setup
+
+# get author, version and license from package files
+# since it has some not yet installed dependencies, parsing the file:
+package = 'stscraper'
+head = open(os.path.join(package, '__init__.py')).read(2048)
+pattern = r"""__%s__\s*=\s*['"]([^'"]*)['"]"""
+kwargs = {keyword: re.search(pattern % keyword, head).group(1)
+          for keyword in ('version', 'author', 'license')}
 
 requirements = [
     line.strip()
@@ -10,13 +20,7 @@ requirements = [
 # options reference: https://docs.python.org/2/distutils/
 # see also: https://packaging.python.org/tutorials/distributing-packages/
 setup(
-    # whenever you're updating the next three lines
-    # please also update oscar.py
     name="strudel.scraper",
-    version="0.2.6",  # make sure to update __init__.py.__version__
-    author='Marat (@cmu.edu)',
-    author_email='marat@cmu.edu',
-
     description="Python interfaces to Github, Bitbucket and Gitlab APIs",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -27,18 +31,16 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: POSIX :: Linux',
         'Topic :: Scientific/Engineering'
     ],
     platforms=["Linux", "Solaris", "Mac OS-X", "Unix", "Windows"],
-    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
-    scripts=['scripts/check_gh_limits.py'],
-    # entry_points={'console_scripts': [
-    #     'console_scripts = stscraper.github:print_limits',
-    # ]},
-    packages=['stscraper'],
-    license="GPL v3.0",
+    python_requires='>2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    scripts=[os.path.join('scripts', 'check_gh_limits.py')],
+    packages=[package],
     url='https://github.com/cmustrudel/strudel.scraper',
-    install_requires=requirements
+    install_requires=requirements,
+    **kwargs
 )
